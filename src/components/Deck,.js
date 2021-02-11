18823b6dc94899642e342ab0e084ebaa689e5b4b
Ariq Dhia Irfanudin <invanteris@gmail.com>
2021-02-11 21:24:25 +0700

    Deck component

--- src/components/Deck,.js
+++ src/components/Deck,.js
@@ -1,47 +1,56 @@
 import React from "react";
 import { StyleSheet, Text, View, Animated } from "react-native";
+import { Card, Button, Image } from "react-native-elements";
 
 const Deck = () => {
 	const DATA = [
 		{
 			id: 1,
 			text: "Card #1",
-			uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1517137708685-c42f99521535?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8c3RyZWV0JTIwbWFya2V0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 		{
 			id: 2,
 			text: "Card #2",
-			uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1524042645623-eea64b3c0370?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8c3RyZWV0JTIwbWFya2V0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 		{
 			id: 3,
 			text: "Card #3",
-			uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1518978478775-2915324171f5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHN0cmVldCUyMG1hcmtldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 		{
 			id: 4,
 			text: "Card #4",
-			uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1524584830732-b69165ddba9a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fHN0cmVldCUyMG1hcmtldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 		{
 			id: 5,
 			text: "Card #5",
-			uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1506917980821-213681597089?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHN0cmVldCUyMG1hcmtldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 		{
 			id: 6,
 			text: "Card #6",
-			uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1525294065345-1708669da7b5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzR8fHN0cmVldCUyMG1hcmtldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 		{
 			id: 7,
 			text: "Card #7",
-			uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1568882041008-c0954e91caba?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzl8fHN0cmVldCUyMG1hcmtldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 		{
 			id: 8,
 			text: "Card #8",
-			uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg",
+			uri:
+				"https://images.unsplash.com/photo-1494795817488-f572f2a30c87?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDR8fHN0cmVldCUyMG1hcmtldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
 		},
 	];
 
@@ -49,9 +58,19 @@ const Deck = () => {
 		<View>
 			{DATA.map((item) => {
 				return (
-					<Text style={styles.text} key={DATA.id}>
-						{item.text}
-					</Text>
+					<Card title={item.text} style={styles.card} key={item.id}>
+						<Image
+							source={{ uri: item.uri }}
+							resizeMode="cover"
+							style={styles.image}
+						/>
+						<Text style={{ marginBottom: 10 }}>{item.text}</Text>
+						<Button
+							buttonStyle={styles.button}
+							icon={{ name: "code" }}
+							title="View now!"
+						/>
+					</Card>
 				);
 			})}
 		</View>
@@ -61,7 +80,10 @@ const Deck = () => {
 export default Deck;
 
 const styles = StyleSheet.create({
-	text: {
-		color: "white",
+	image: {
+		height: 150,
+	},
+	button: {
+		backgroundColor: "brown",
 	},
 });
